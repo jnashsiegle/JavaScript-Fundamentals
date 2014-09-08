11c98359767f6ec7fd0d3e4f62b15681c021bf86
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-08 11:44:26 -0500

    Declaring / Defining Variables
    Screencast: Declaring Variables (necessary) Approx. Duration: 6m
    How to set up and use variables with the machine.
    
    Screencast: Defining Variables (necessary) Approx. Duration: 10m
    How to store values in your variables as you use them.

--- wk2Videos/js/script.js
+++ wk2Videos/js/script.js
@@ -2,9 +2,24 @@
  * Created by jsiegle on 9/8/14 for Video Assignments Week 2
  */
 
-var yearBorn = 1962;
+/*var yearBorn = 1962;
 
 console.log("Hello World!");
 console.log(yearBorn);
 
-alert(yearBorn);
+alert(yearBorn);*/
+
+
+
+// Defining Variables
+
+var age;  //declaration
+age =  52;  //definition
+
+var yearOfBirth = 1980;  //declaring and defining on same line
+
+
+
+
+
+
