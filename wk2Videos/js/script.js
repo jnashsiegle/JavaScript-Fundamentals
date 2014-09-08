f3c3a352de5f7a2ea9f11f3644aca20ac5e3f1d8
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-08 12:17:38 -0500

    Arrays
    Screencast: Arrays (necessary) Approx. Duration: 16m
    More complex than variables, arrays handle collections of data. Learn
    to use array access notation to access items within arrays.

--- wk2Videos/js/script.js
+++ wk2Videos/js/script.js
@@ -12,12 +12,36 @@ alert(yearBorn);*/
 
 
 // Defining Variables
-
+/*
 var age;  //declaration
 age =  52;  //definition
 
 var yearOfBirth = 1980;  //declaring and defining on same line
+*/
 
+//Variables of Different Types, Numbers, Booleans and Strings
+
+
+/*
+var age = 38; // Number Variable
+
+var isStudent = false; //Boolean Variable (on or off, true or false
+
+var firstName = "Kermit";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the Professor'; //using escape operators (I'd just use double quotes all the time for consistency)
+*/
+
+// Arrays
+
+var muppetNames = ["Kermit" , "Miss Piggy"]; //could also be 38 or firstName (var) ];  0, 1 , 2 , 3 etc are index numbers for the array
+
+muppetNames[1] = "Rizzo";
+muppetNames[2] = "Fozzy";
+
+var num = 1;
+
+console.log(muppetNames[num]); //how would you include Kermit, Miss Piggy?or how would you list 1 and 2?
 
 
 
