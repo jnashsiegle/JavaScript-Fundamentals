a577e82895b8efa12fcf958808332fb8e2dd241b
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-08 13:58:04 -0500

    Expressions pt. 1
    Screncast: Basic Expressions (necessary) Approx. Duration: 6m
    
    Screencast: Example Expressions (necessary) Approx. Duration: 6m
    
    Screencast: Arithmetic Operators (necessary) Approx. Duration: 10m
    How to add, subtract and perform other calculations with the machine.
    
    Screencast: Using Modulo (helpful) Approx. Duration: 6m
    How to use remainders to evaluate numbers.

--- wk2Videos/js/script.js
+++ wk2Videos/js/script.js
@@ -32,7 +32,7 @@ var firstName = "Kermit";
 var phrase = 'I don\'t know \n You\'ll have to speak with the Professor'; //using escape operators (I'd just use double quotes all the time for consistency)
 */
 
-// Arrays
+/* Arrays
 
 var muppetNames = ["Kermit" , "Miss Piggy"]; //could also be 38 or firstName (var) ];  0, 1 , 2 , 3 etc are index numbers for the array
 
@@ -42,6 +42,37 @@ muppetNames[2] = "Fozzy";
 var num = 1;
 
 console.log(muppetNames[num]); //how would you include Kermit, Miss Piggy?or how would you list 1 and 2?
+
+*/
+
+//Expressions - Age Example
+
+/* var yearBorn = 1962;
+
+
+2014 - yearBorn;
+var age = 2014 - yearBorn;
+
+console.log(age);
+*/
+
+/*Area of a triangle is 1/2W * H
+
+var width = 8;
+var height = 7;
+var area = width/2 * height;
+
+console.log(area)
+*/
+
+/*Modulo
+
+var remainder = 211 % 2;  //replace division / with % sign for Modulo to get remainder only; useful for determining even/odd numbers.
+
+console.log(remainder);
+*/
+
+
 
 
 
