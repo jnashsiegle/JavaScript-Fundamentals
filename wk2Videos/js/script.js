b1f16948fc20847eb5aa3c6f544c310e1b22c114
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-08 15:02:13 -0500

    Expressions Pt 2
    Screencast: Order of Operations (necessary) Approx. Duration: 12m
    Just like in mathmatics, calculations are performed in a certain order.
    Learn about this order and how to work with it in this video.
    
    Screencast: Using Arrays in Expressions (necessary) Approx. Duration: 5m
    This video covers using array access notation to use items stored
    within arrays in calculations.
    
    Screencast: Concatenating Strings (necessary) Approx. Duration: 11m
    Working with strings in expressions is a different animal than working
    with numbers in mathmatical expressions.
    
    Screencast: Casting (necessary) Approx. Duration: 12m
    How to tell the computer to treat numbers and strings and vice versa.
    
    Screencast: Assignment Operators (necessary) Approx. Duration: 12m
    Calculating with AND assigning all on one line with these handy
    programming shortcut operators.
    
    Also found part of answer to Kermit/Miss Piggy question.

--- wk2Videos/js/script.js
+++ wk2Videos/js/script.js
@@ -32,18 +32,24 @@ var firstName = "Kermit";
 var phrase = 'I don\'t know \n You\'ll have to speak with the Professor'; //using escape operators (I'd just use double quotes all the time for consistency)
 */
 
-/* Arrays
+//Arrays
 
-var muppetNames = ["Kermit" , "Miss Piggy"]; //could also be 38 or firstName (var) ];  0, 1 , 2 , 3 etc are index numbers for the array
+/*var muppetNames = ["Kermit" , "Miss Piggy"]; //could also be 38 or firstName (var) ];  0, 1 , 2 , 3 etc are index numbers for the array
 
 muppetNames[1] = "Rizzo";
 muppetNames[2] = "Fozzy";
 
-var num = 1;
+//var num = 1;
 
-console.log(muppetNames[num]); //how would you include Kermit, Miss Piggy?or how would you list 1 and 2?
+//Adding Concatenating from below partially answered my question
+var result = muppetNames[1] + "," +" " +  muppetNames[2] + "," + " " + "Miss Piggy" ;
+
+console.log(result); //how would you include Kermit, Miss Piggy?or how would you list 1 and 2?
 
 */
+
+
+
 
 //Expressions - Age Example
 
@@ -72,9 +78,87 @@ var remainder = 211 % 2;  //replace division / with % sign for Modulo to get rem
 console.log(remainder);
 */
 
+//Expressions - PEMDAS - The Order of Operations Please Excuse My Aunt Daisy
+// Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
+/*
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by the number of items
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+
+console.log(average);
+*/
+
+/*var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter)
+
+//Don't overuse parentheses...just use them if Order of Operations effects outcome.
+*/
+//Using Arrays in Expressions
+/*
+var orangeBins = [234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+
+console.log(total);
+
+*/
+
+//Concatenating
+
+/*var firstName = "Jana";
+var lastName = "Nash-Siegle";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);*/
+/*
+var a = "6";
+var b = "7";
+
+var result = a + b;
+console.log(result);
+
+*/
+
+//Casting
+
+/*
+var stringVar = "6";
+ /*
+var result = 7 + Number(stringVar);
+console.log(result);
+
+
+var areaCode = 407;
+var firstPart = 203;
+var secPart = 4529;
+
+//(407) 203-4529
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
+console.log(phoneNo);
+
+*/
+//Assignment Operators
+
+var a = 3;
+//a += 4;  //same as a = a + 4;
+//a *= 4;
+//a /= 4;  //  *=  and /=  and -= all work the same as +=
+//a -= 4;
+
+//a ++ ;  //a = a + 1 // a += 1
+
+a --; //a = a -1 //a -=1
+
+console.log(a);
 
 
 
-
-
+a
 
