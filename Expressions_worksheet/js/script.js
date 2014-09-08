74ef3500de3d45612495c4dcf81e510932c1c5ab
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-08 16:35:34 -0500

    Slice of Pizza pt 1
    A bunch of students are having a party and somebody ordered pizza.
    Create an expression that calculates how much pizza
      each partygoer will get at the party.  (Assume all pizzas have the
    same number of slices and that the person dividing
       the pizza is really precise, so this can be a decimal, like 3.52
    slices, etc.)

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -16,7 +16,8 @@
  Don't forget!
  Test your code! Do the calculations on your own calculator. Check that the computer is producing the same result.
  Commit often!
- A "given" is a variable that is part of the initial set up of a formula. You should write your code so anyone can change the values of the givens and the result of the calculation should still be accurate.
+ A "given" is a variable that is part of the initial set up of a formula. You should write your code so anyone can
+ change the values of the givens and the result of the calculation should still be accurate.
 
 
 /* Dog Years
@@ -24,6 +25,8 @@
    Calculate how old Sparky the pit bull is in dog years based on his actual age.
  */
 
+
+/*
 //Givens are Sparky's age and dog years
 var sparkyAge = 5;
 var dogYears = 7;
@@ -32,6 +35,28 @@ var dogYears = 7;
 x = sparkyAge * dogYears;
 
 console.log("Sparky is " + sparkyAge + " " + "human years old which is " + x + " " +  "in dog years.");
+
+
+
+// Slice of Pizza pt 1
+ A bunch of students are having a party and somebody ordered pizza. Create an expression that calculates how much pizza
+  each partygoer will get at the party.  (Assume all pizzas have the same number of slices and that the person dividing
+   the pizza is really precise, so this can be a decimal, like 3.52 slices, etc.)
+*/
+
+
+   // Givens of people and pizza
+
+   var slicePizza = 8;
+   var people = 40;
+   var wholePizza = 10;
+
+//Result variable
+
+x = (slicePizza * wholePizza) / people;
+
+console.log("Each person ate " + x + " slices of pizza at the party." );
+
 
 
 
