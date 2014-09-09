da9b233457a3802b9bc5e4b6d988492540a0ed73
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-09 18:24:06 -0500

    Assignment: Expression (Personal)
    The beginning of my Personal expression assignment including 3
    variables, 3 math calculations, 1 prompt and 1 alert.

--- Assignment: Expressions/Personal/js/script.js
+++ Assignment: Expressions/Personal/js/script.js
@@ -1,2 +1,24 @@
 /**
  * Jana Nash-Siegle 9/9/2014 Personal**/
+
+
+//How much dog food will I need in order to feed one dog for one year and how much will it cost?
+//The dog food comes in 15 pound bags, the serving size is 2 cups and it costs $29.99 per bag.
+    //There are 4 cups to the pound
+
+var userInput = prompt("How many pounds of dog food do I use a month?");
+
+var result = userInput/15;
+var total = result * 12;
+
+var cost = total * 29.99;
+
+alert("If I use " + userInput + " pounds of dog food a month, then I buy " + result + " bag(s) a month or " + total +
+    " bags " + " per year. \n Each bag costs $29.99 so I spend $" + cost + " a year on dog food.");
+
+console.log(result, total, cost);
+
+
+
+
+
