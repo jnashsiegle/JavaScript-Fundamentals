0f41c83ca86f9a8e34fa07e611f3ec7d232e4f20
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-09 10:59:13 -0500

    Prompts and Alerts
    Following along with the Prompts and alerts videos.

--- wk2Videos/js/script.js
+++ wk2Videos/js/script.js
@@ -159,5 +159,25 @@ a --; //a = a -1 //a -=1
 console.log(a);
 */
 
+// Alerts and Prompts
+/*
+var userInput = prompt("Enter your year of birth:");
+console.log(userInput);
+
+*/
+
+//Calculate area of rectangle
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.")
+;
+var height = prompt("Please enter the height.");
+var area = width * height;
+//console.log(area);
+
+var result = "The area of your rectangle is " + area + " sq feet";
+
+alert(result);
+
+
 
 
