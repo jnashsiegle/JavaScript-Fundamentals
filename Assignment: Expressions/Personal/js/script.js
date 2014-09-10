61b5f3ce90d2bbc93c940477b95ed4914fe5f503
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-09 22:56:43 -0500

    Assignment: Expression  (Personal)
    Added if statement and skip() to opening prompt asking if a dog is
    owned.  If no, an alert box apologizes for not being able to help.  If
    yes, it continues on to the dog food.

--- Assignment: Expressions/Personal/js/script.js
+++ Assignment: Expressions/Personal/js/script.js
@@ -8,28 +8,20 @@
 
 
     var userInput1 =  prompt("Do you own a dog?");
-        if ( userInput1 == "no");
-        alert("I'm sorry we can't help you.");
-        {
-            skip();
-        }
 
-        if ( userInput1 == "yes");
-        {
-            continue;
-        }
-
-var userInput = prompt("How many pounds of dog food do I use a month?");
-
-
+if (userInput1 != "no") {
+    var userInput = prompt("How many pounds of dog food do I use a month?");
+} else {
+    alert("I'm sorry we can't help you");
+    skip();
+}
 var result = userInput/15;
 var total = result * 12;
-
 var cost = total * 29.99;
-
+{
 alert("If I use " + userInput + " pounds of dog food a month, then I buy " + result + " bag(s) a month or " + total +
     " bags " + " per year. \n Each bag costs $29.99 so I spend $" + cost + " a year on dog food.");
-
+}
 console.log(result + " bags a month.  ", total + " bags per year.  ", "I spend $" + cost + " a year on dog food.");
 
 
