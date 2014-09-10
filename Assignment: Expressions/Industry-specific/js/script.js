4c9a47b5b04bb3c74a304a1222ceea4839aac548
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-10 13:36:28 -0500

    Expression: Assignment - Industry Specific
    Added request for name and added text to console.logs for all prompts
    so far.
    Added variables to code for userPhone and userEmail.

--- Assignment: Expressions/Industry-specific/js/script.js
+++ Assignment: Expressions/Industry-specific/js/script.js
@@ -9,18 +9,29 @@ What is your phone number?
 
 Thank you, we shall be in touch with you shortly.
  */
-var services = ["SEO"];
+var services = ["SEO"];             //array listing of services to pull into prompts and console
 
  services[1] = "web design";
  services[2] = "mobile apps";
  services[3] = "hosting";
 
-prompt("Welcome, how may we help you today?  \n We offer the best in " + services[0] + ", " + services[1] +  ", " + services[2]
+var userInput = prompt("Welcome, how may we help you today?  \n We offer the best in " + services[0] + ", " + services[1] +  ", "
+    + services[2]
     + ", and " + services[3] + ".\n Please type the " +
     "service you would like more information on.");
 
-console.log("Welcome, how may we help you today?  \n We offer the best in " + services[0] + ", " + services[1] +  ", " + services[2]
+console.log("Welcome, how may we help you today?  \n We offer the best in " + services[0] + ", " + services[1] +  ", "
+    + services[2]
     + ", and " + services[3] + ". \n Please type the service you would like more information on.");
+console.log("You would like more information on " + userInput + ".");
+
+var userName = prompt("So we may contact you, please give us your name");
+var userPhone;
+var userEmail;
+
+
+
+console.log("Your name is " + userName + ".");
 
 
 
