941e18ab98953e191fb7be5ee5fe04896b0d3106
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-10 18:13:28 -0500

    Assignment: Expression - Industry Specific
    Final Project - added console.log only math equations filled in by user
    prompts but no alerts.

--- Assignment: Expressions/Industry-specific/js/script.js
+++ Assignment: Expressions/Industry-specific/js/script.js
@@ -18,14 +18,36 @@ var services = ["SEO"];             //array listing of services to pull into pro
  services[2] = "mobile apps";
  services[3] = "hosting";
 
+//Offer Services
+
 var userInput = prompt("Welcome, how may we help you today?  \n We offer the best in " + services[0] + ", " +
     services[1] +  ", "    + services[2]    + ", and " + services[3] + ".\n Please type the " +
     "service you would like more information on.");
 
 console.log("Welcome, how may we help you today?  \n We offer the best in " + services[0] + ", " + services[1] +  ", "
-    + services[2]
-    + ", and " + services[3] + ". \n Please type the service you would like more information on.");
+    + services[2] + ", and " + services[3] + ". \n Please type the service you would like more information on.");
 console.log("You would like more information on " + userInput + ".");
+
+//for mathematical equations in console.log ONLY
+
+var ageCompany = prompt("Please take a small survey for us. \n How old is your company?");
+var empHire = prompt("How many employees do you hire a year?");
+var currentEmp = prompt("How many employees do you currently employ?");
+var hoursWork = prompt("How many hours a week do they work?");
+var years = 20;
+
+var futureCompany = Number(ageCompany) + years;
+console.log("In " +  years +  " years your company will be " + futureCompany + " years old.");
+
+var newHire = Number(empHire) * years;
+console.log("In " + years + " years you will hire " + newHire + " new employees." );
+
+var currentHours = Number(currentEmp) * Number(hoursWork);
+console.log("Your " + currentEmp + " employees currently work " + currentHours + " hours a week.");
+
+//end console.log ONLY questions
+
+
 
 var userName = prompt("So we may contact you, please give us your name");
 
