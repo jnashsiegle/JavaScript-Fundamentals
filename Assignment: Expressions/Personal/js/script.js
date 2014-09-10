24b71432c975144b2aac698cc2cd278d128a09e0
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-10 11:20:40 -0500

    Assignment: Expressions - Personal
    Finished - Need one resolution to a step I’m not happy with,
    but I have to move on at this point.  For now, it is rigged up to work
    using a skip() statement.  Not pretty, but shows green in WebStorm,
    throws red error in console, but the script works correctly.  Will come
    back to see if I can resolve this after I complete other exercises. In
    real world this would definitely need resolved before launch.

--- Assignment: Expressions/Personal/js/script.js
+++ Assignment: Expressions/Personal/js/script.js
@@ -9,20 +9,27 @@
 
     var userInput1 =  prompt("Do you own a dog?");
 
-if (userInput1 != "no") {
-    var userInput = prompt("How many pounds of dog food do I use a month?");
-} else {
+        userInput1.toLowerCase();
+        console.log("Thank you for your answer of " + userInput1);
+
+if (userInput1.toLowerCase() == "no") {
     alert("I'm sorry we can't help you");
-    skip();
+
+    skip();                                      //stops script from running on a no reply.  There has to be a better way
+                                                //of doing this, but it's working for this assignment despite error.
+                                       }
+else if (userInput1.toLowerCase() == "yes") {
+    var userInput = prompt("How many pounds of dog food do you use a month?");
+
 }
 var result = userInput/15;
 var total = result * 12;
 var cost = total * 29.99;
 {
-alert("If I use " + userInput + " pounds of dog food a month, then I buy " + result + " bag(s) a month or " + total +
-    " bags " + " per year. \n Each bag costs $29.99 so I spend $" + cost + " a year on dog food.");
+alert("If you use " + userInput + " pounds of dog food a month, then you buy " + result + " bag(s) a month or " + total +
+    " bags " + " per year. \n Each bag costs $29.99 so you spend $" + cost + " a year on dog food.");
 }
-console.log(result + " bags a month.  ", total + " bags per year.  ", "I spend $" + cost + " a year on dog food.");
+console.log(result + " bags a month.  ", total + " bags per year.  ", "You spend $" + cost + " a year on dog food.");
 
 
 
