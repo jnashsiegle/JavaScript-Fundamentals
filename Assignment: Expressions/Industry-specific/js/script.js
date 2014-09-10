e10cd4c6416427af49b6fa28ad02c4a93ab0257d
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-10 12:46:40 -0500

    Assignment: Expression - Industry Specific
    Beginning of script code/idea

--- Assignment: Expressions/Industry-specific/js/script.js
+++ Assignment: Expressions/Industry-specific/js/script.js
@@ -1,2 +1,20 @@
 /**
  * Jana Nash-Siegle 9/9/2014 Industry-Specific**/
+
+
+/*How can we help you today? Web Design, Mobile Apps, Hosting
+So we can contact you regarding your request, what is your name?
+What is your email?
+What is your phone number?
+
+Thank you, we shall be in touch with you shortly.
+ */
+
+var userInput = prompt("Welcome, how may we help you today?  \n We off the best in Web Design, mobile apps, and " +
+    "hosting. \n Please type the service you would like more information on.");
+
+console.log(userInput);
+
+
+
+
