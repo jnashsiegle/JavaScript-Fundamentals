ebac1b023be83fa92890baecdc8e1c7647b5f81e
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-10 13:15:19 -0500

    Assignment: Expression - Industry Specific
    Added array to use in prompt and console.log

--- Assignment: Expressions/Industry-specific/js/script.js
+++ Assignment: Expressions/Industry-specific/js/script.js
@@ -9,11 +9,18 @@ What is your phone number?
 
 Thank you, we shall be in touch with you shortly.
  */
+var services = ["SEO"];
 
-var userInput = prompt("Welcome, how may we help you today?  \n We off the best in Web Design, mobile apps, and " +
-    "hosting. \n Please type the service you would like more information on.");
+ services[1] = "web design";
+ services[2] = "mobile apps";
+ services[3] = "hosting";
 
-console.log(userInput);
+prompt("Welcome, how may we help you today?  \n We offer the best in " + services[0] + ", " + services[1] +  ", " + services[2]
+    + ", and " + services[3] + ".\n Please type the " +
+    "service you would like more information on.");
+
+console.log("Welcome, how may we help you today?  \n We offer the best in " + services[0] + ", " + services[1] +  ", " + services[2]
+    + ", and " + services[3] + ". \n Please type the service you would like more information on.");
 
 
 
