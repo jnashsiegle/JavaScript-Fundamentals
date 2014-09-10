cf2b75e9ce03a8e246bf2e4cbd30d72c2325b2eb
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-10 17:35:11 -0500

    Assignment: Expressions - personal
    Asked about skip() in class, understand resolution and implemented if
    else statement correctly

--- Assignment: Expressions/Personal/js/script.js
+++ Assignment: Expressions/Personal/js/script.js
@@ -11,32 +11,39 @@
 //The dog food comes in 15 pound bags, the serving size is 2 cups and it costs $29.99 per bag.
     //There are 4 cups to the pound
 
+//find out if they own a dog
+    var wantDog =  prompt("Do you own a dog?  Yes or no?");
 
-    var userInput1 =  prompt("Do you own a dog?");
+        //wantDog.toLowerCase();
+        console.log("Thank you for your answer of " + wantDog);
 
-        userInput1.toLowerCase();
-        console.log("Thank you for your answer of " + userInput1);
+//find out how many pounds of dog food they buy a month
 
-if (userInput1.toLowerCase() == "no") {
-    alert("I'm sorry we can't help you");
-
-    skip();                                      //stops script from running on a no reply.  There has to be a better way
-                                                //of doing this, but it's working for this assignment despite error.
-                                       }
-else if (userInput1.toLowerCase() == "yes") {
+if (wantDog.toLowerCase() === "yes") {
     var userInput = prompt("How many pounds of dog food do you use a month?");
+    var result = userInput/15;
+    var total = result * 12;
+    var cost = total * 29.99;
 
-}
-var result = userInput/15;
-var total = result * 12;
-var cost = total * 29.99;
-{
-alert("If you use " + userInput + " pounds of dog food a month, then you buy " + result + " bag(s) a month or " + total +
-    " bags " + " per year. \n Each bag costs $29.99 so you spend $" + cost + " a year on dog food.");
-}
-console.log(result + " bags a month.  ", total + " bags per year.  ", "You spend $" + cost + " a year on dog food.");
+    //tell user the calculations
+    {
+        alert("If you use " + userInput + " pounds of dog food a month, then you buy " + result + " bag(s) a month or "
+            + total + " bags " + " per year. \n Each bag costs $29.99 so you spend $" + cost + " a year on dog food.");
+    }
+    console.log(result + " bag(s) a month.  ", total + " bag(s) per year.  ", "You spend $" + cost + " a year on dog " +
+        "food.");
 
+    //find out what type of dog they own
 
+var breedDog = prompt("What type of dog do you own?");
+    alert("The " + breedDog + " is a very cute dog.");
+    console.log("The " + breedDog + " is a very cute dog.")
+                                       }
+else {
+
+    alert("I'm sorry we can't help you as you don't buy dog food.");
+    console.log("I'm sorry we can't help you as you don't buy dog food.");
+       }
 
 
 
