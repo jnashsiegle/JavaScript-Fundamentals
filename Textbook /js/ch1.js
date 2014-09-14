17e7a70080cc4e6834c971097a60b6e4554ac970
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-14 16:36:07 -0500

    Textbook Examples
    Chapter 1, worked some of the script via external js.  bgColor via
    external js was not working.  It’s deprecated anyway, so unsure why
    book teaches it.  left it on index html.  Found the stepping of
    paragraphs interesting.

---
+++ Textbook /js/ch1.js
@@ -0,0 +1,15 @@
+/**
+ * Jana Nash-Siegle
+ * WPF
+ * Textbook Tryouts
+ * 9/14/14.
+ */
+
+//alert("testing, testing, 1,2,3")
+
+window.onload = function() {   //unlike textbook I don't want script on index page.  switched to working via external
+                                //script link
+    document.getElementById("ResultsP").innerHTML = "Hello World!";
+
+};
+console.log("hello");
---
+++ Textbook /js/ch2.js
@@ -0,0 +1,6 @@
+/**
+ * Jana Nash-Siegle
+ * WPF
+ * Textbook Tryouts
+ * 9/14/14.
+ */
