d29c7aef749ac5b6d560f62fcbde9fb2f94bd218
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-16 12:11:26 -0500

    conditionals_Worksheet
    initial files and example complete

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,43 @@
+//Jana Nash-Siegle 9/8/2014 Conditionals Worksheet
+
+/*
+ or each solution you will need the following:
+ Label the section of code appropriately
+ Write the givens as variables
+ The result should be calculated using variables, not literal values when possible.
+ Create code that will make decisions based on the value of the givens
+ Print the result of the decision-making to the console.
+ Use only code and techniques learned in this class.
+ Example:
+ Problem: If the temperature is less than 75 degrees we will go to the beach, otherwise we will go to the movies.
+ Answer:
+ //Hot Enough?<br> //Is it hot enough to go to the beach?<br> var temp = 80;<br> //if the temperature is less than 75..
+
+ <br> if(temp < 75){<br>      //if its less than 75<br>      console.log(“We will go to the beach!”);<br> }else{<br>
+       //if it’s greater or equal to 75<br>      console.log(“We will go to the movies.”);<br> }
+
+ Example:
+ Stuff your face I
+
+ To get in the heavyweight division of the Strawberry Festival’s pie eating contest you must weigh 250 lbs or more.
+  Determine whether an entrant qualifies based on his weight.
+
+ Given:
+ Competitor’s weight.
+ Result:
+ “The competitor qualifies for the heavyweight division.” Or  “ The competitor needs to gain some weight!”
+
+*/
+
+//Example
+
+var qualWeight = 250;
+
+
+
+if(qualWeight >= "250"){
+    console.log("The competitor qualifies for the heavyweight division.")
+
+}else {
+    console.log("The competitor needs to gain some weight!")
+}
