d63eacd417c5bd6798235ad1feff9a4a0d6ff970
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-17 18:52:43 -0500

    Assignment: Conditionals - Industry
    First Phase of Web Services Calculation finished.

--- Assignment: Conditionals/Industry-specific/js/script.js
+++ Assignment: Conditionals/Industry-specific/js/script.js
@@ -10,20 +10,31 @@
 //alert("testing")
 
 alert("Welcome to ABC Web Development.  A series of prompts will follow this notice, simply answer yes or no if it" +
-    "is a service you would like us to offer you and give you an estimate for.");
+    " is a service you would like us to offer you.  An estimate will follow.");
+
+var cDomainName = 10;
+var cHostSite = 50;
+var cWebPDM = 500;
+
 
 var domainName = prompt("Would you like to register a domain name?"); // ask if service they want, 10.00 charge
-if( domainName == "yes"){
-    console.log("You would like us to register a domain name for you.");
+if( domainName === "yes"){
+    console.log("You would like us to register a domain name for you. \n That will cost " + cDomainName + " dollars.");
+}else{
+    console.log("Let's move on to something else.")
 }
 var hostSite = prompt("Are you interested in hosting services?"); // 50.00 year if yes to hosting
-if(hostSite == "yes"){
-    console.log("You would like us to host a site for you");
+if(hostSite === "yes"){
+    console.log("You would like us to host a site for you. \n That will cost " + cHostSite + " dollars.");
+}else{
+    console.log("Let's move on to something else.")
 }
 var webPDM = prompt("Would you like us to design, develop and maintain a website for you?"); // $500 a year if yes
-if(webPDM = "yes"){
-    console.log("You would like us to design, develop and maintain a website for you.  Great!");
+if(webPDM === "yes"){
+    console.log("You would like us to design, develop and maintain a website for you.  Great! \n Our package costs "
+        + cWebPDM + " dollars.");
+}else{
+    console.log("Let's move on to something else.")
 }
-if(webPDM = "yes"){
-
-}
+//if(domainName = "yes" ||  hostSite = "yes" || webPDM = "yes");
+//    how to add up cDomainName, cHostSite, cWebPDM?
