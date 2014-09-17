789443cceaba9661440709d6fc21f03fafc111d3
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-17 15:57:47 -0500

    Assignment: Conditionals
    Beginning of Web Site Cost Calculations based upon requested services
    that are offered.

--- Assignment: Conditionals/Industry-specific/js/script.js
+++ Assignment: Conditionals/Industry-specific/js/script.js
@@ -7,4 +7,23 @@
  * **/
 
 
-alert("testing")
+//alert("testing")
+
+alert("Welcome to ABC Web Development.  A series of prompts will follow this notice, simply answer yes or no if it" +
+    "is a service you would like us to offer you and give you an estimate for.");
+
+var domainName = prompt("Would you like to register a domain name?"); // ask if service they want, 10.00 charge
+if( domainName == "yes"){
+    console.log("You would like us to register a domain name for you.");
+}
+var hostSite = prompt("Are you interested in hosting services?"); // 50.00 year if yes to hosting
+if(hostSite == "yes"){
+    console.log("You would like us to host a site for you");
+}
+var webPDM = prompt("Would you like us to design, develop and maintain a website for you?"); // $500 a year if yes
+if(webPDM = "yes"){
+    console.log("You would like us to design, develop and maintain a website for you.  Great!");
+}
+if(webPDM = "yes"){
+
+}
