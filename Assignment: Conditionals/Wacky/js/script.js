fcdb23996697423b9e0985945363b431b7a06c52
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-18 16:15:38 -0500

    Assignment: Conditionals - Random Number game
    Final Upload for Random Number Game.
    Fixed While Loop
    Changed var userInput = 0 so prompts default to 0,
    no need to check empty string once initial greeting prompt has been
    responded to.
    removed var=6 used for testing purposes.
    fixed a few misplaced (’s.

--- Assignment: Conditionals/Wacky/js/script.js
+++ Assignment: Conditionals/Wacky/js/script.js
@@ -5,7 +5,7 @@ Jana Nash-Siegle
 Assignment: Conditionals - Wacky */
 
 //alert("testing testing 1, 2 3");
-var badGuess;
+var userInput = 0;
 
 var playGame = prompt("Welcome!  Do you want to play the Random Number Game?"); //Greet and ask if want to play
 while(playGame === "") {
@@ -15,32 +15,25 @@ while(playGame === "") {
 if(playGame.toLowerCase() === "yes"  || playGame.toLowerCase() === "y"){  //making any input compatible
     console.log("Great!  Let's go!");
 
-
-    var randomNumber=Math.floor(Math.random()*101);  //Have game choose a random number
+    var randomNumber = Math.floor(Math.random()*101);  //Have game choose a random number
+    //randomNumber = 6;   // for testing.   take out when script works correctly
     console.log(Number(randomNumber));
 
 
-    var userInput = prompt("Guess a number between 1 and 100."); // have user guess a number
-    console.log("You guessed " + Number(userInput) + ".");
-
-    {
-
+    while (userInput != randomNumber) {
+         userInput = prompt("Guess a number between 1 and 100."); // have user guess a number
+         console.log("You guessed " + Number(userInput) + ".");
 
         if (Number(userInput) === Number(randomNumber)) {
             console.log("Fantastic!  " + Number(userInput) + " is correct!  You got it right!");  //This block is for correct answer
             alert("Fantastic!  " + Number(userInput) + " is correct!  You got it right!");
 
-        } else(badGuess = ("Try Again! Your guess is to " + ((Number(userInput) < Number(randomNumber)) ? " low." :
-            " high.")));
-        {//This ternary evaluates incorrect guess as to whether it is too low or high
-
+        } else { badGuess = "Try Again! Your guess is to " + (Number(userInput) < Number(randomNumber) ? ' low.' : ' high.');
+            // This ternary evaluates incorrect guess as to whether it is too low or high
             console.log(badGuess);
-            prompt(badGuess);
+            alert(badGuess);
         }
     }
-
-
- }else{
-    console.log("Awww I'm sorry to see you go!");
+} else {
+    console.log("Awww I'm sorry to see you go!"); //say goodnight Gracie!
 }
-
