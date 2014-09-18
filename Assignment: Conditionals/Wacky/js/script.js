dd4d6c4b15f2a78032fa2bbbfd70e6b7f42e1218
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-18 10:47:25 -0500

    Assignment: Conditionals - Wacky
    Beginning of Random Number Game

--- Assignment: Conditionals/Wacky/js/script.js
+++ Assignment: Conditionals/Wacky/js/script.js
@@ -6,3 +6,13 @@ Assignment: Conditionals - Wacky */
 
 //alert("testing testing 1, 2 3");
 
+var playGame = prompt("Welcome!  Do you want to play the Random Number Game?"); //Greet and ask if want to play
+while(playGame === "") {
+    playGame = prompt("You forgot to input something!");  //checking for empty string
+}
+
+if(playGame.toLowerCase() === "yes"  || playGame.toLowerCase() === "y"){  //making any input compatible
+    console.log("Great!  Let's go!");
+ }else{
+    console.log("Awww I'm sorry to see you go!");  //How to stop here if no, with no further script execution
+}
