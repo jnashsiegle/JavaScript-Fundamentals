9d439d47d5cf5f4eab3d57394ec3dd8253ba83ae
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-17 21:53:13 -0500

    Assignment: Conditionals
    Added Final Total Wording to console.log with
    console.log(“Your final total for services is (sum = cDomainName +
    cHosting + cWeb) + “ dollars.);

--- Assignment: Conditionals/Industry-specific/js/script.js
+++ Assignment: Conditionals/Industry-specific/js/script.js
@@ -19,24 +19,23 @@ var cWebPDM = 0;
 
 var domainName = prompt("Would you like to register a domain name?"); // ask if service they want, 10.00 charge
 if( domainName === "yes"){
-    console.log("You would like us to register a domain name for you. \n That will cost " + cDomainName + " dollars.");
+    console.log("You would like us to register a domain name for you. ");
     cDomainName = 10;
 }else{
     console.log("Let's move on to something else.")
 }
 var hostSite = prompt("Are you interested in hosting services?"); // 50.00 year if yes to hosting
 if(hostSite === "yes"){
-    console.log("You would like us to host a site for you. \n That will cost " + cHostSite + " dollars.");
+    console.log("You would like us to host a site for you. ");
     cHostSite = 50;
 }else{
     console.log("Let's move on to something else.")
 }
 var webPDM = prompt("Would you like us to design, develop and maintain a website for you?"); // $500 a year if yes
 if(webPDM === "yes"){
-    console.log("You would like us to design, develop and maintain a website for you.  Great! \n Our package costs "
-        + cWebPDM + " dollars.");
+    console.log("You would like us to design, develop and maintain a website for you.  Great! ");
     cWebPDM = 500
 }else{
     console.log("Let's move on to something else.")
 }
-console.log("Your total fee for services would come to " + cDomainName + cHostSite + cWebPDM);
+console.log("Your total fee for services would come to " + (sum = cDomainName + cHostSite + cWebPDM) + " dollars.");
