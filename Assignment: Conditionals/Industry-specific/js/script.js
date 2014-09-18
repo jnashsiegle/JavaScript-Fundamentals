dc7277e8617f370379cefb1fece4699ea29e4d4c
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-17 22:33:11 -0500

    Assignment: Conditionals
    Added .toLowerCase() to validate against y || Yes || YES etc on prompts

--- Assignment: Conditionals/Industry-specific/js/script.js
+++ Assignment: Conditionals/Industry-specific/js/script.js
@@ -21,7 +21,7 @@ var domainName = prompt("Would you like to register a domain name?"); // ask if 
     while(domainName === "") {
         domainName = prompt("You forgot to input something!");  // checking for empty string, re-running prompt
     }
-        if (domainName === "yes") {
+        if(domainName.toLowerCase()  === "yes" || domainName.toLowerCase() === "y") {
             console.log("You would like us to register a domain name for you. ");
             cDomainName = 10;
         } else {
@@ -31,7 +31,7 @@ var hostSite = prompt("Are you interested in hosting services?"); // 50.00 year 
     while(hostSite === "") {
         hostSite = prompt("You forgot to input something!");  // checking for empty string, re-running prompt
     }
-        if (hostSite === "yes") {
+        if (hostSite.toLowerCase() === "yes" || hostSite.toLowerCase() === "y") {
             console.log("You would like us to host a site for you. ");
             cHostSite = 50;
         } else {
@@ -41,10 +41,11 @@ var webPDM = prompt("Would you like us to design, develop and maintain a website
     while(webPDM === "") {
         webPDM = prompt("You forgot to input something!");  // checking for empty string, re-running prompt
     }
-        if (webPDM === "yes") {
+        if (webPDM.toLowerCase() === "yes" || webPDM.toLowerCase() === "y") {
             console.log("You would like us to design, develop and maintain a website for you.  Great! ");
             cWebPDM = 500
         } else {
             console.log("Let's move on to something else.")
         }
-        console.log("Your total fee for services would come to " + (sum = cDomainName + cHostSite + cWebPDM) + " dollars.");
+        console.log("Your total fee for services would come to " + (sum = cDomainName + cHostSite + cWebPDM) +
+            " dollars.");
