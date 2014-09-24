982a8c26dfbe04234b5c804e34c171f34fb1c5e2
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-24 18:01:24 -0500

    Assignment: Functions - Personal
    Cash or Credit

--- Assignment: Functions/Personal/js/script.js
+++ Assignment: Functions/Personal/js/script.js
@@ -35,7 +35,11 @@ function carCount() {
     return returnCar;
 }
 
-console.log(passengers);
+console.log("We have " + passengers + " family members going.");
 
+var cash = prompt("Will we pay for gas with cash or credit?");
+while(cash=== ""){
+    cash = prompt("You forgot to answer the question. \n Please say cash or credit.");
+}
 
 
