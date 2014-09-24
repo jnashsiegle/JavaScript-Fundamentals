8b2278d2773734926f324e41b55a953442784442
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-24 14:39:01 -0500

    Assignment: Functions
    Personal, working on how many cars do we need, script.

--- Assignment: Functions/Personal/js/script.js
+++ Assignment: Functions/Personal/js/script.js
@@ -7,4 +7,18 @@
  * **/
 
 
-/
+//alert("testing, testing");
+
+var passengers = prompt("How many people will be going on the trip?", "enter a value between 1-10");  //How many people are going?
+while(passengers === "") {
+    passengers = prompt("You forgot to input something!");
+}
+var carCount = function(passengers){
+    if(passengers <= 5){
+        carCount("one car");
+    }
+}
+
+
+
+
