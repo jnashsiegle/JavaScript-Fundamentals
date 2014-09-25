641ee343cc39f4c57fae27e29801e18b5a2f0de6
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-25 11:05:27 -0500

    Assignment: Functions - Wacky
    Dice Game - Predict the number the die will roll.

--- Assignment: Functions/Wacky/js/script.js
+++ Assignment: Functions/Wacky/js/script.js
@@ -6,3 +6,29 @@ Assignment: Functions - Wacky */
 
 //alert("testing testing 1, 2 3");
 
+
+
+var userInput = prompt("Input a number between 1 and 36");
+
+
+
+
+function rollDie(){
+    var die = Math.floor(Math.random() *6) +1;
+    console.log(die);
+
+
+
+
+
+
+
+}
+
+console.log("This is our random number " + rollDie() + "but you picked " + userInput);
+
+
+//var returnDie = rollDie();
+
+
+
