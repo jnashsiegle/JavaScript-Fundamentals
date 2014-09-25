042059bf0d1be8879d9f328743cd7aa89f358189
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-24 22:18:31 -0500

    Assignment: Functions
    second run at the script

--- Assignment: Functions/Industry-specific/js/script.js
+++ Assignment: Functions/Industry-specific/js/script.js
@@ -8,5 +8,39 @@
 
 //Basic Color Converter
 
-var colorArray = ["red", "blue", "green", "white", "black"];  //color choices
+/*var colorNames = ["Red", "Blue", "Green", "White", "Black"];  //color choices
+var element = 0;
 var hexArray = ["#FF0000", "#0000FF", "008000", "#FFFFFF", "#000000"];  //matching hex codes
+
+var userChoice = prompt("Choose a color:  " + colorNames);
+
+var returnHex = colorConverter();
+colorConverter();
+
+
+function colorConverter(){
+
+    return
+
+    console.log("You chose " + userChoice + " and the correct hex code is");
+}
+
+console.log("The color is " + colorNames[0] + " so the hex code is " + hexArray[0]);*/
+
+
+
+var userChoice = prompt("Choose a color:  " + colorName);
+findHex(userChoice);
+
+function findHex(userChoice){
+
+
+    var colorName = ["Red", "Blue", "Green", "White", "Black"];  //color choices
+    var element = 0;
+
+    while(colorName[element] != userChoice){
+        element = element +1;
+    }
+
+    return colorName[element];
+}
