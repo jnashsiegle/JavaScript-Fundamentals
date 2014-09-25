e0c5ff870a0342a20a0537d3928ca903433e9414
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-09-25 14:24:59 -0500

    Assignment Function - Final Uploads

--- Assignment: Functions/Industry-specific/js/script.js
+++ Assignment: Functions/Industry-specific/js/script.js
@@ -14,24 +14,25 @@ function findHex(userChoice){
 
     var colorName = ["Red", "Blue", "Green", "White", "Black"];  //color choices
     var hexCode = ["#FF0000", "#0000FF", "008000", "#FFFFFF", "#000000"];  //matching hex codes
-    var element = 0;
+    var element = 0;  //default color element to 0
 
-    while(colorName[element] != userChoice){
+    while(colorName[element] != userChoice){ //this compares the colorName to user Input
         element = element +1;
-    }
+    }                             // AND
 
-    return hexCode[element];
+    return hexCode[element];   //and returns the hexcode
 
 
 }
 
-var userChoice = prompt("Choose a color:  " + " Red, Blue, Green, White, or Black.");
+var userChoice = prompt("Choose a color:  " + " Red, Blue, Green, White, or Black.");  //prompt user for color
 
 while(userChoice === ""){
     userChoice = prompt("Please don't leave blank.  \n Choose a color:  " + " Red, Blue, Green, White, or Black.")
-}
-console.log("Choose a color:  " + " Red, Blue, Green, White, or Black.");
-var hexNum = findHex(userChoice);
+}     //VALIDATION
 
-console.log("The hexcode for " + userChoice + " is " + hexNum + ".");
+console.log("Choose a color:  " + " Red, Blue, Green, White, or Black.");   //print out of prompt to console
+var hexNum = findHex(userChoice);      // This equates hexnum to findHex function
+
+console.log("The hexcode for " + userChoice + " is " + hexNum + ".");   //prints out statement to console.
 
--- Assignment: Functions/Wacky/js/script.js
+++ Assignment: Functions/Wacky/js/script.js
@@ -6,37 +6,39 @@ Assignment: Functions - Wacky */
 
 //alert("testing testing 1, 2 3");
 
-var min = 2;
-var userInput = prompt("Input a number between 1 and 12");
-while(userInput === ""  || isNaN(userInput )) {
+var min = 1;  //minimal number die can total
+var userInput = prompt("Input a number between 2 and 12");  //prompt for user guess
+while(userInput === ""  || isNaN(userInput )) {   //validate user response to number's only no blanks
 
 
     //reprompt user
     userInput = prompt("Please don't leave blank or type in anything but numbers. \n Please type in a number");
 }
 
-rollDie(min, userInput);
+rollDie(min, userInput);  //invoke function
 
 
 
 
 
-function rollDie(min, userInput) {
+function rollDie(min, userInput) {  //function to calculate total of two rolled die and compare to user number
 
 
-    var die1 = Math.floor(Math.random() * 6) + min;
-    var die2 = Math.floor(Math.random() * 6) + min;
-    var dieTotal = die1 + die2;
+    var die1 = Math.floor(Math.random() * 6) + min;   //first die roll
+    var die2 = Math.floor(Math.random() * 6) + min;   //second die roll
+    var dieTotal = die1 + die2;    //total of two die
 
 
-    var response = (userInput == dieTotal)? "Win"  : "Lose" ;
-    alert(response);
+    var response = (userInput == dieTotal)? "Win"  : "Lose" ;  //alert to win or lose
+    alert(response);  //send alert
+    console.log(response);  //send via console.log
 
-    //console.log("You rolled " + dieTotal);
+
 
 
 
     console.log("The dice rolled a total number of " + dieTotal  + " and you picked " + userInput);
+    //summarize number picked and number rolled
 
 
 }
